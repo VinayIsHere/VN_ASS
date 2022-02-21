b1edf532507f130b0cf8e9dee1e258e99cf679c5
nalam9257 <50151327+nalam9257@users.noreply.github.com>
2022-02-22 03:24:10 +0530

    company_Symbols
    macros (symbols)

---
+++ stockComSymbols.py
@@ -0,0 +1,97 @@
+# 
+# import enum
+# class StockSymbols(enum.Enum):
+#     Adani_Port_and_Special_Economic_Zone=1
+#     Asian_Paints_Ltd=2;
+#     Axis_Bank_Ltd=3;
+#     Bajaj_Auto_Ltd=4;
+#     Bajaj_Finance_Ltd=5;
+#     Bharat_Petroleum_Corporation_Ltd=6;
+#     Bhart_Infratel_Ltd=7;
+#     Cipla_Ltd=8;
+#     Coal_India_Ltd=9;
+#     Dr_Reddys_Laboratories_Ltd=10;
+#     Eicher_Motors_Ltd=11;
+#     Gail_Ltd=12;
+#     Grasim_Industries_Ltd=13;
+#     Hcl_Technologies_Ltd=14;
+#     HDFC_Bank_Ltd=15;
+#     Hero_MotoCorp_Ltd=16;
+#     Hindalco_industries_Ltd=17;
+#     Hindustan_Petroleum_Corporation_Ltd=18;
+#     Hindustan_Unilever_Ltd=19;
+#     Housing_Development_Finance_Corporation_Ltd=20;
+#     ITC_Ltd=21;
+#     ICICI_Bank=22;
+#     Indiabulls_Housing_Finance_Ltd=23;
+#     Indian_Oil_corporation_Ltd=24;
+#     Induslnd_Bank_Ltd=25;
+#     Infosys_Ltd=26;
+#     Kotak_Mahindra_Bank_Ltd=27;
+#     Larsen_And_Toubro_Ltd=28;
+#     Lupin_Ltd=29;
+#     Mahindra_And_Mahindra_Ltd=30;
+# 
+# symbols={StockSymbols.Adani_Port_and_Special_Economic_Zone:'ASIANPAINT',
+#     StockSymbols.Asian_Paints_Ltd:"ASIANPAINT",
+#     StockSymbols.Axis_Bank_Ltd:"AXISBANK",
+#     StockSymbols.Bajaj_Auto_Ltd:"BAJAJ-AUTO",
+#     StockSymbols.Bajaj_Finance_Ltd:"BAJFINANCE",
+#     StockSymbols.Bharat_Petroleum_Corporation_Ltd:"BPCL",
+#     StockSymbols.Bhart_Infratel_Ltd:"INFRATEL",
+#     StockSymbols.Cipla_Ltd:"CIPLA",
+#     StockSymbols.Coal_India_Ltd:"COALINDIA",
+#     StockSymbols.Dr_Reddys_Laboratories_Ltd:"DRREDDY",
+#     StockSymbols.Eicher_Motors_Ltd:"EICHERMOT",
+#     StockSymbols.Gail_Ltd:"GAIL",
+#     StockSymbols.Grasim_Industries_Ltd:"GRASIM",
+#     StockSymbols.Hcl_Technologies_Ltd:"HCLTECH",
+#     StockSymbols.HDFC_Bank_Ltd:"HDFCBANK",
+#     StockSymbols.Hero_MotoCorp_Ltd:"HEROMOTOCO",
+#     StockSymbols.Hindalco_industries_Ltd:"HINDALCO",
+#     StockSymbols.Hindustan_Petroleum_Corporation_Ltd:"HINDPETRO",
+#     StockSymbols.Hindustan_Unilever_Ltd:"HINDUNILVR",
+#     StockSymbols.Housing_Development_Finance_Corporation_Ltd:"HDFC",
+#     StockSymbols.ITC_Ltd:"ITC",
+#     StockSymbols.ICICI_Bank:"ICICIBANK",
+#     StockSymbols.Indiabulls_Housing_Finance_Ltd:"IBULHSGFIN",
+#     StockSymbols.Indian_Oil_corporation_Ltd:"IOC",
+#     StockSymbols.Induslnd_Bank_Ltd:"INDUSINDBK",
+#     StockSymbols.Infosys_Ltd:"INFY",
+#     StockSymbols.Kotak_Mahindra_Bank_Ltd:"KOTAKBANK",
+#     StockSymbols.Larsen_And_Toubro_Ltd:"LT",
+#     StockSymbols.Lupin_Ltd:"LUPIN",
+#     StockSymbols.Mahindra_And_Mahindra_Ltd:"M&M"};
+
+
+
+symbol={"Adani_Port_and_Special_Economic_Zone":'ASIANPAINT',
+    "Asian_Paints_Ltd":"ASIANPAINT",
+    "Axis_Bank_Ltd":"AXISBANK",
+    "Bajaj_Auto_Ltd":"BAJAJ-AUTO",
+    "Bajaj_Finance_Ltd":"BAJFINANCE",
+    "Bharat_Petroleum_Corporation_Ltd":"BPCL",
+    "Bhart_Infratel_Ltd":"INFRATEL",
+    "Cipla_Ltd":"CIPLA",
+    "Coal_India_Ltd":"COALINDIA",
+    "Dr_Reddys_Laboratories_Ltd":"DRREDDY",
+    "Eicher_Motors_Ltd":"EICHERMOT",
+    "Gail_Ltd":"GAIL",
+    "Grasim_Industries_Ltd":"GRASIM",
+    "Hcl_Technologies_Ltd":"HCLTECH",
+    "HDFC_Bank_Ltd":"HDFCBANK",
+    "Hero_MotoCorp_Ltd":"HEROMOTOCO",
+    "Hindalco_industries_Ltd":"HINDALCO",
+    "Hindustan_Petroleum_Corporation_Ltd":"HINDPETRO",
+    "Hindustan_Unilever_Ltd":"HINDUNILVR",
+    "Housing_Development_Finance_Corporation_Ltd":"HDFC",
+    "ITC_Ltd":"ITC",
+    "ICICI_Bank":"ICICIBANK",
+    "Indiabulls_Housing_Finance_Ltd ":"IBULHSGFIN",
+    "Indian_Oil_corporation_Ltd":"IOC",
+    "Induslnd_Bank_Ltd":"INDUSINDBK",
+    "Infosys_Ltd":"INFY",
+    "Kotak_Mahindra_Bank_Ltd":"KOTAKBANK",
+    "Larsen_And_Toubro_Ltd":"LT",
+    "Lupin_Ltd":"LUPIN",
+    "Mahindra_And_Mahindra_Ltd":"M&M"};
