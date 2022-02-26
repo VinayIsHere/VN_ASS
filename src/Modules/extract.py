5e43c7c7cc1da583fb3aa06b5977b95866ee3639
vinayKs21 <vinaykumar.shukla#contraly.com>
2022-02-26 17:06:28 +0530

    mean and median function modified and verified

--- src/Modules/extract.py
+++ src/Modules/extract.py
@@ -1,36 +1,42 @@
 import pandas as pd;
 import numpy as np;
 import scipy as sp;
+
+def calCulateProfitAndLoss(dataframe, months):
+    return list((dataframe["Close"].head(months*30)-dataframe["Open"].head(months*30)))
+
 def avgFun(dataframe,week,months):
     sum=0;
     days=week*7;
     avgList=[];
-    arrProfitAndLoss=list((dataframe["Close"].head(months*30)-dataframe["Open"].head(months*30)))
-    length=len(arrProfitAndLoss);
-    for i in range(length):
-        sum+=arrProfitAndLoss[i];
-        if i!=0:
-            if i%days==0:
-                avg=sum/days;
-                avgList.append(avg);
+    arrProfitAndLoss= calCulateProfitAndLoss(dataframe, months)
 
-                sum=0;
+    length=len(arrProfitAndLoss);
+    
+    sum=0   
+    for i in range(1, length+1):
+        sum+=arrProfitAndLoss[i-1];
+        
+        if(i%days == 0):
+            avgList.append(sum/days)
+            sum=0
 
     return avgList;
 
 
 def median(dataframe,week,months):
-    arrProfitAndLoss = list((dataframe["Close"].head(months * 30) - dataframe["Open"].head(months * 30)))
+    arrProfitAndLoss = calCulateProfitAndLoss(dataframe, months)
     length = len(arrProfitAndLoss);
     days=week*7
     median=[]
-    for i in range(length):
+    for i in range(1,length+1, 14):
         temp = []
-        temp.append(arrProfitAndLoss[i]);
-        if i!=0:
-            if i%days==0:
-                medn=np.median(temp)
-                median.append(medn);
+        temp.extend(arrProfitAndLoss[i-1: i-1+14]);
+        #numpy will do the sorting by itself
+        medn=np.median(temp)
+        median.append(medn);
+        
+
     return  median
 
 
