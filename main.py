9dd56178094d1ac4030c49c6908cf7f4ffb33842
vinayKs21 <vinaykumar.shukla#contraly.com>
2022-03-19 23:21:25 +0530

    main updated by having generic path and tested the newly added wrapper to pta

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import sys
-sys.path.insert(0, '/home/noor/Desktop/VN_ASS/MACROS')
-sys.path.insert(1, '/home/noor/Desktop/VN_ASS/src/Modules')
-from stockComSymbols import symbols, StockSymbols
+from MACROS.stockComSymbols import symbols, StockSymbols
+from src.Modules.analysis import rsi, macd, superTrend
+from src.Modules.statistics import mean, median  
+from src.Modules import extract
 from nsepy import get_history
-import extract
-import src.Modules.rsicalculation as rsiCal
 from datetime import date
 
 def main():
-    data= get_history(symbols[StockSymbols.Cipla_Ltd], date(2021, 9, 23), date(2022, 2, 1))
+    data= get_history(symbols[StockSymbols.Cipla_Ltd], date(2022, 1, 1), date(2022, 3, 19))
     # print(data);
     # avg=[];
     # months=3;
     # weeks=2;
-    # avg=extract.avgFun(data,weeks,months);
+    # avg=mean.mean(data,weeks,months);
     # print(avg);
-    rsi=rsiCal.RSI_pandas_ta(data,14)
-    print(rsi);
-
-
+    rsiCal=rsi.rsi(data,14)
+    macdCal= macd.macd(data, 12, 26, 9)
+    print(rsiCal)
+    print("-------------")
+    print(macdCal)
+    print("-------------")
+    #call supertrend wrapper
+    
 if __name__ == "__main__":
     main()
